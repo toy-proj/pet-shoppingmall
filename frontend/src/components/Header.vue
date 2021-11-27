c19be9ba77f4cd7016fb9440aeed54445c630ecf
eunjin0212 <eunjin9639@gmail.com>
2021-11-27 17:48:55 +0900

    nav bar 만들기

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg">
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">menu</b-nav-item>
+          <b-nav-item href="#">즐겨찾기</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">로그인</b-nav-item>
+          <b-nav-item href="#">회원가입</b-nav-item>
+          <b-nav-item href="#">장바구니</b-nav-item>
+          <b-nav-item href="#">마이페이지</b-nav-item>
+          <b-nav-item href="#">고객센터</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <div class="row">
+      <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
